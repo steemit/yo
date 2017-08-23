701c40bdd5242b328bcb718ac56a7ecbceef18c8
Gareth Nelson <gareth@steemit.com>
2017-08-24 00:27:19 +0200

    add API endpoint and import jsonrpc_auth

--- yo/api_server.py
+++ yo/api_server.py
@@ -11,23 +11,10 @@ import json
 import logging
 logger = logging.getLogger(__name__)
 
-def needs_posting_key(func):
-    def new_func(*args,**kwargs):
-        if not 'authsig' in kwargs.keys():
-           return {'error':'This method needs authorisation with your posting key'}
-        if not 'raw_json' in kwargs.keys():
-           return {'error':'Internal error!'}
-        digest  = hashlib.sha256(kwargs['raw_json']).digest()
-        account = Account(kwargs['username'])
-        posting_keys = []
-        for auth in account['posting']['key_auths']: posting_keys.append(auth[0])
-        # TODO - implement auth verification here
-    return func
-
 class YoAPIServer(YoBaseService):
    service_name='api_server'
    q = asyncio.Queue()
    async def api_update_preferences(self,username=None,new_preferences={},raw_json=None):
        pass
    async def async_task(self,yo_app):
-       self.api_methods['update_preferences'] = needs_posting_key(self.api_update_preferences)
+       self.api_methods['update_preferences'] = self.api_update_preferences
--- yo/app.py
+++ yo/app.py
@@ -7,8 +7,30 @@ import asyncio
 import logging
 logger = logging.getLogger(__name__)
 
+from jsonrpcserver.aio import methods
+from jsonrpc_auth.AuthorizedRequest import verify_request_with_pub
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+from jsonrpcserver.aio import methods
+
+def add_api_method(func,func_name):
+    methods.add(func,name='yo.%s' % func_name)
+
+async def handle_api(request):
+      req_app = request.app
+      request = await request.json()
+      orig_request = request
+      logger.debug('Incoming request: %s' % request)
+      if not 'params' in request.keys(): request['params'] = {} # fix for API methods that have no params
+      request['params']['yo_app']    = req_app['config']['yo_app']
+      request['params']['yo_db']     = req_app['config']['yo_db']
+      request['params']['yo_config'] = req_app['config']['yo_config']
+      request['params']['orig_req']  = orig_request # needed for authentication
+      response = await methods.dispatch(request)
+      return web.json_response(response)
+
+
 class YoApp:
    def __init__(self,config=None,db=None):
        self.config=config
@@ -19,7 +41,8 @@ class YoApp:
        self.web_app = web.Application(loop=self.loop)
        self.web_app['config'] = {
           'yo_config':self.config,
-          'yo_db'    :self.db
+          'yo_db'    :self.db,
+          'yo_app'   :self
        }
        self.running = False
    async def start_background_tasks(self,app):
@@ -27,21 +50,25 @@ class YoApp:
        for k,v in self.service_tasks.items():
            logger.info('Starting %s' % k)
            self.web_app['service_task:%s' % k] = self.web_app.loop.create_task(v(self))
+   async def api_healthcheck(self,**kwargs):
+       return({'status':'OK'})
+   async def setup_standard_api(self,app):
+       add_api_method(self.api_healthcheck,'healthcheck')
+       self.web_app.router.add_post('/', handle_api)
    def run(self):
        self.running = True
        self.web_app.on_startup.append(self.start_background_tasks)
+       self.web_app.on_startup.append(self.setup_standard_api)
        web.run_app(self.web_app,
                    host=self.config.get_listen_host(),
                    port=self.config.get_listen_port())
    def add_service(self,service):
        name = service.get_name()
        for k,v in service.api_methods.items():
-           self.api_methods['yo.%s' % name] = v # add public API methods
+           add_api_method(v,k)
        self.service_tasks[name] = service.async_task
        service.yo_app = self
        self.services[name]=service
-   async def invoke_public_api(self,service,api_method,*args,**kwargs):
-
    async def invoke_private_api(self,service,api_method,*args,**kwargs):
        # TODO - add support for URLs other than :local:
        if not service in self.services.keys():
