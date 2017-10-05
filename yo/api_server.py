f365ca811a9e7ec2e76c67cc41e04fff51611514
Gareth Nelson <gareth@steemit.com>
2017-10-05 23:51:20 +0200

    update get_notifications params and add mark_read

--- yo/api_server.py
+++ yo/api_server.py
@@ -49,21 +49,35 @@ class YoAPIServer(YoBaseService):
                             'notify_type'   :row.notify_type,
                             'sub_data'      :row.sub_data})
          return retval
-   async def api_get_notifications(self,username=None,since=None,orig_req=None,yo_db=None,**kwargs):
+   async def api_get_notifications(self,username=None,created_before=None,updated_after=None,read=None,notify_type=None,orig_req=None,yo_db=None,**kwargs):
        """ Get all notifications since the specified time
 
        Keyword args:
           username(str): The username to query for
-          since(str):    The iso8601 formatted timestamp to query since
+          created_before(str): ISO8601-formatted timestamp
+          updated_after(str): ISO8601-formatted timestamp
+          read(bool): If set, only returns notifications with read flag set to this value
+          notify_type(str): The notification type to return
 
        Returns:
-          list: list of notifications
+          list: list of notifications represented in dictionary format
        """
        return [mock_notification()]
+   async def api_mark_read(self,notification_ids=[],orig_req=None,yo_db=None,**kwargs):
+       """ Mark a list of notifications as read
+
+       Keyword args:
+           notification_ids(list): List of notifications to mark read
+       
+       Returns:
+           list: list of notifications updated
+       """
+       return []
    async def api_test_method(self,**kwargs):
        return {'status':'OK'}
    async def async_task(self,yo_app): # pragma: no cover
        yo_app.add_api_method(self.api_enable_transports,'enable_transports')
        yo_app.add_api_method(self.api_get_enabled_transports,'get_enabled_transports')
        yo_app.add_api_method(self.api_get_notifications,'get_notifications')
+       yo_app.add_api_method(self.api_mark_read,'mark_read')
        yo_app.add_api_method(self.api_test_method,'api_test_method')
