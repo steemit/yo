fa7c141eda071cd31d98cbb7c0ddce47afe66b36
Gareth Nelson <gareth@steemit.com>
2017-10-26 01:51:10 +0200

    parse start_block setting to allow specifying head block or n blocks back

--- yo/blockchain_follower.py
+++ yo/blockchain_follower.py
@@ -55,7 +55,17 @@ class YoBlockchainFollower(YoBaseService):
                 return op
        return None
    async def async_ops(self,loop,b):
-       ops = b.stream_from(start_block=int(self.yo_app.config.config_data['blockchain_follower']['start_block']) )
+       start_block = str(self.yo_app.config.config_data['blockchain_follower'].get('start_block',''))
+       # turn the start_block into something understandable to steem-python:
+       # blank value is None
+       # negative values are the head block minus that amount
+       if start_block=='':
+          start_block = None # TODO: at some point go back and implement the block_status thing for multiple blockchain followers
+       else:
+          start_block = int(start_block) # TODO: handle malformed config in the config module and spit out appropriate errors
+          if start_block < 0:
+             start_block = b.get_current_block_num() - start_block
+       ops = b.stream_from(start_block=start_block)
        while True:
            next_val = None
            try:
