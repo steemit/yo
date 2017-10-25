67b62ff4190f0bcd237a97f3a98f79412c467bee
Gareth Nelson <gareth@steemit.com>
2017-10-25 23:19:58 +0200

    tests/test_api_server.py::test_api_get_notifications_mysql PASSED

--- tests/test_api_server.py
+++ tests/test_api_server.py
@@ -6,17 +6,25 @@ from yo import config
 from yo import db
 from yo import api_server
 
-source_code_path = os.path.dirname(os.path.realpath(__file__))
+from _test_utils import MySQLServer
 
-def get_mockdata_db():
+source_code_path = os.path.dirname(os.path.realpath(__file__))
+no_docker = pytest.mark.skipif(os.getenv('INDOCKER','0')=='1',reason='Does not work inside docker')
+mysql_test = pytest.mark.skipif(os.getenv('SKIPMYSQL','0')=='1',reason='Skipping MySQL tests')
+
+def get_mockdata_db(use_mysql=False,mysql_params={}):
     """Returns a new instance of YoDatabase backed by sqlite3 memory with the mock data preloaded"""
-    yo_config = config.YoConfigManager(None,defaults={'database':{'provider'   :'sqlite',
-                                                                  'init_schema':'1',
-                                                                  'init_data'  :'%s/../data/mockdata.json' % source_code_path},
-                                                                  'sqlite':{'filename':':memory:'}})
+    db_defaults = {'database':{'init_data'  :'%s/../data/mockdata.json' % source_code_path,
+                               'init_schema':'1'}}
+    if use_mysql:
+       db_defaults['database']['provider'] = 'mysql'
+       db_defaults['mysql']                = mysql_params
+    else:
+       db_defaults['database']['provider'] = 'sqlite'
+       db_defaults['sqlite']               = {'filename':':memory:'}
+    yo_config = config.YoConfigManager(None,defaults=db_defaults)
     yo_db = db.YoDatabase(yo_config)
     return yo_db
-
 
 @pytest.mark.asyncio
 async def test_api_test_method():
@@ -33,13 +41,27 @@ async def test_api_get_notifications():
       some_notifications = await API.api_get_notifications(username='test_user',test=True,limit=5)
       assert len(some_notifications)==5
 
-
-
 @pytest.mark.asyncio
-async def test_api_get_notifications_real():
-      """Basic test of get_notifications backed by real DB"""
+async def test_api_get_notifications_sqlite():
+      """Basic test of get_notifications backed by sqlite3"""
       API = api_server.YoAPIServer()
       API.testing_allowed = False # we only want real DB data
       db = get_mockdata_db()
       some_notifications = await API.api_get_notifications(username='test_user',limit=5,yo_db=db)
+      assert len(some_notifications)==5
+
+@no_docker
+@mysql_test
+@pytest.mark.asyncio
+async def test_api_get_notifications_mysql():
+      """Basic test of get_notifications backed by MySQL"""
+      API = api_server.YoAPIServer()
+      API.testing_allowed = False # we only want real DB data
+
+      server = MySQLServer(db_name='yo_test',db_user='yo_test',db_pass='1234')
+      server.wait()
+      db = get_mockdata_db(use_mysql=True,mysql_params={'username':'yo_test','password':'1234','database':'yo_test'})
+
+      some_notifications = await API.api_get_notifications(username='test_user',limit=5,yo_db=db)
+      server.stop()
       assert len(some_notifications)==5
